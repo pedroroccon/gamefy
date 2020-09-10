e6d1873ec4ec1ca77918e9024aa60d9719e1fcbf
Pedro Roccon <pedro@pedroroccon.com.br>
2020-09-10 18:15:17 +0000

    FIX: Get correct number of Recently Reviewed from API

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -40,8 +40,8 @@ class GamesController extends Controller
                 fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating, rating_count, summary;
                 where platforms = (48,49,130,6) 
                 & (first_release_date >= {$before}
-                & first_release_date < {$current});
-                & rating_count > 5;
+                & first_release_date < {$current}
+                & rating_count > 5);
                 sort popularity desc;
                 limit 3;
             "
