741a77abfca1898d402baafe3698d229ebc61cd6
Pedro Roccon <pedro@pedroroccon.com.br>
2020-09-10 13:24:33 +0000

    Making the index page responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 	<div class="container mx-auto px-4">
 		<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
 
-		<div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+		<div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
 			<div class="game mt-8">
 				<div class="relative inline-block">
 					<a href="#">
@@ -21,8 +21,8 @@
 				<div class="text-gray-400 mt-1">Playstation 4</div>
 			</div>
 		</div>
-		<div class="flex my-10">
-			<div class="recently-reviewed w-3/4 mr-32">
+		<div class="flex flex-col lg:flex-row my-10">
+			<div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
 				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
 				<div class="recently-reviewed-container space-y-12 mt-8">
 					<div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
@@ -36,17 +36,17 @@
 							</div>
 						</div>
 					
-						<div class="ml-12">
+						<div class="ml-6 lg:ml-12">
 							<a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">Sample game name</a>
 							<text class="gray-400 mt-1">Playstation 4, Xbox One</text>
 
-							<p class="mt-6 text-gray-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum beatae, deleniti est quos harum omnis sint delectus porro facere quidem, doloremque magnam rem, accusantium blanditiis sequi cum veniam ipsam perferendis.</p>
+							<p class="mt-6 text-gray-400 hidden lg:block">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum beatae, deleniti est quos harum omnis sint delectus porro facere quidem, doloremque magnam rem, accusantium blanditiis sequi cum veniam ipsam perferendis.</p>
 
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="game-sidebar w-1/4">
+			<div class="game-sidebar lg:w-1/4 mt-12 lg:mt-0">
 				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most anticipated</h2>
 				<div class="most-anticipated-container space-y-10 mt-8">
 					<div class="game flex">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,18 +13,18 @@
     <body class="bg-gray-900 text-white">
 
         <header class="border-b border-gray-800">
-            <nav class="container mx-auto flex items-center justify-between px-4 py-6">
-                <div class="flex items-center">
+            <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+                <div class="flex flex-col lg:flex-row items-center">
                     <a href="{{ url('/') }}"><img src="{{ asset('svgs/logo.svg') }}" alt="{{ env('APP_NAME') }}" class="w-32 flex-none"></a>
 
-                    <ul class="flex ml-16 space-x-8">
+                    <ul class="flex ml-0 lg:ml-16 space-x-8 mt-6 lg:mt-0">
                         <li><a href="#" class="hover:text-gray-400">Games</a></li>
                         <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
                         <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
                     </ul>
                 </div>
 
-                <div class="flex items-center">
+                <div class="flex items-center mt-6 lg:mt-0">
                     <div class="relative">
                         <input type="text" class="bg-gray-800 text-sm rounded-full focus:outline-none focus:shadow-outline w-64 px-3 pl-8 py-1" placeholder="Search game...">
 
