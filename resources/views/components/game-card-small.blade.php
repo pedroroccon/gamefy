1acb022dd80993c07768974da34805107e983715
Pedro Roccon <pedro@pedroroccon.com.br>
2021-01-16 16:54:56 -0300

    Better date formats

--- resources/views/components/game-card-small.blade.php
+++ resources/views/components/game-card-small.blade.php
@@ -4,6 +4,6 @@
 	</a>
 	<div class="ml-4">
 		<a href="{{ route('games.show', $game->slug) }}" class="hover:text-gray-300">{{ $game->name }}</a>
-		<div class="text-gray-400 text-sm mt-1">{{ ! empty($game->first_release_date) ? $game->first_release_date->format('Y-m-d') : 'Not defined' }}</div>
+		<div class="text-gray-400 text-sm mt-1">{{ ! empty($game->first_release_date) ? $game->first_release_date->format('Y, M d') : 'Not defined' }}</div>
 	</div>
 </div>
